cb5cc8b17fa6f4336c0809ced85fd7c578f381ad
jeanettejohnson <johnsonje197@gmail.com>
2022-07-29 11:47:55 -0400

    wip

--- python-loader/my_clickablempl.py
+++ python-loader/my_clickablempl.py
@@ -4,12 +4,22 @@ import matplotlib.pyplot as plt
 from pyMCDS import pyMCDS
 from matplotlib.widgets import Button
 from matplotlib.text import Annotation
+from matplotlib.patches import ConnectionPatch
 import numpy as np
 from adjustText import adjust_text
 
-def annotate(axis, text, x, y):
+def annotate(axis, text, x, y, parentx, parenty):
     text_annotation = Annotation(text, xy=(x, y), xycoords='data')
     axis.add_artist(text_annotation)
+    if(parentx != -1):
+        xyA = (x, y)
+        xyB = (parentx, parenty)
+        coordsA = "data"
+        coordsB = "data"
+        con = ConnectionPatch(xyA, xyB, coordsA, coordsB,
+                              arrowstyle="-|>", shrinkA=0, shrinkB=0,
+                              mutation_scale=10, fc="w")
+        axis.add_artist(con)
 # define the behaviour -> what happens when you pick a dot on the scatterplot by clicking close to it
 def onpick(event):
     # step 1: take the index of the dot which was picked
@@ -24,15 +34,33 @@ def onpick(event):
 
     # if the dots are to close one to another, a list of dots clicked is returned by the matplotlib library
     for i in ind:
+        id = ids[int(i)]
+        parentid = parents[int(i)]
+        if(parentid >= 0):
+            parentpos = ids.index(int(parentid))
+        else:
+            parentpos = 0
         # step 3: take the label for the corresponding instance of the data
-        label = "me: "+str(ids[i])+"\nparent: "+str(parents[i])
-        annotate(
-            ax,
-            label,
-            label_pos_x + offset,
-            label_pos_y + offset
-        )
-
+        if parentid != -1:
+            label = "me: "+str(id)+"\nparent: "+str(parentid)
+            annotate(
+                ax,
+                label,
+                label_pos_x + offset,
+                label_pos_y + offset,
+                positionx[parentpos],
+                positiony[parentpos]
+            )
+        else:
+            label = "me: "+str(id)+"\nparent: none"
+            annotate(
+                ax,
+                label,
+                label_pos_x + offset,
+                label_pos_y + offset,
+                -1,
+                -1
+            )
         # step 6: force re-draw
         # adjust_text(label, expand_points=(1, 1), expand_text=(1, 1),
         #             arrowprops=dict(arrowstyle="-", color='black', lw=0.5))
@@ -44,7 +72,7 @@ def onpick(event):
 
 
 def test_saving_parent_ID():
-    mcds1 = pyMCDS('output00000015.xml', '../PhysiCell_V_1.10.4/output')
+    mcds1 = pyMCDS('output00000020.xml', '../PhysiCell_V_1.10.4/output')
     #print(mcds1.get_cell_df())
     print()
     cell_df = mcds1.get_cell_df()
@@ -69,11 +97,13 @@ points = []
 # plt.show()
 fig,ax = plt.subplots()
 cid = fig.canvas.mpl_connect('pick_event', onpick)
-mcds1 = pyMCDS('output00000015.xml', '../PhysiCell_V_1.10.4/output')
+mcds1 = pyMCDS('output00000020.xml', '../PhysiCell_V_1.10.4/output')
 cell_df = mcds1.get_cell_df()
 points = []
 parents=list(cell_df["parent_ID"])
 ids =list(cell_df["ID"])
+positionx = list(cell_df["position_x"])
+positiony = list(cell_df["position_y"])
 test_saving_parent_ID()
 plt.show()
 
